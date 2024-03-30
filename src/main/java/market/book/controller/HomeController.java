853dbefd95696408f3bea7f5aadf10070ef97ddd
chang-seop <ikavon@naver.com>
2024-03-30 19:07:12 +0900

    Chore: 프로젝트 이름 변경

--- src/main/java/market/book/controller/HomeController.java
+++ src/main/java/market/book/controller/HomeController.java
@@ -1,4 +1,4 @@
-package changseop.core.controller;
+package market.book.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
--- src/test/java/market/book/BookApplicationTests.java
+++ src/test/java/market/book/BookApplicationTests.java
@@ -1,10 +1,10 @@
-package changseop.core;
+package market.book;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CoreApplicationTests {
+class BookApplicationTests {
 
 	@Test
 	void contextLoads() {
